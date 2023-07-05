5f55c7388ab0bc9bcf2c4412b972e54010ed454d
msa <msai.sadehi@gmail.com>
2023-07-05 21:59:37 +0330

    second

---
+++ session2.py
@@ -0,0 +1,101 @@
+# print(type(1))
+# print(type(1.5))
+
+# print(1 + 2)
+# print(1 - 2)
+# print(1 / 2)
+# print(1 * 2)
+
+# print(4 / 2)
+# print(4 // 2)
+
+# print(4 % 2)
+# print(5 % 2)
+
+
+# x = 12
+
+# print(x)
+
+# x = "saber"
+# x = 'saber'
+# print(x)
+
+# y = ''
+# y = ""
+# y = 'a'
+# y = "a"
+# y = 'aaaa'
+# y = "aaaa"
+# print(type(y))
+
+
+# 1name = "reza" # error
+# name1 = "reza"
+# name_1 = "saber"
+# _name_1 = "sara"
+
+
+# name_one = "artin"
+# name-one = "javad" # error
+
+
+# print(2 + 3 * 2)
+# print((2 + 3) * 2)
+
+
+# x = 2
+# z = 5
+
+# result = 2 * x + 3 * z
+# print(result)
+
+
+# message = 'hello every body'
+# print(message)
+
+
+# message = "welcome to python class"
+# print(message)
+
+
+# message = "hello every body. \nwelcome to python class. \nwe are going to learn every things about python."
+# print(message)
+
+# message = """hello every body. 
+# welcome to python class. 
+# we are going to learn every things about python."""
+# print(message)
+
+# message = '''hello every body. 
+# welcome to python class. 
+# we are going to learn every things about python.'''
+# print(message)
+
+
+# message = "He said \"some thing\""
+# print(message)
+
+# message = 'He said "some thing"'
+# print(message)
+
+# name = "artin"
+# course = "java"
+# message = f"hello {name}. welcome to {course} class."
+
+# print(message)
+
+
+# name = "sara"
+# course = "django"
+# message = "hello " + name + " welcome to " + course + " class."
+# print(message)
+
+name = input("Enter your name: ")
+print(name)
+
+
+# TODO    برنامه ای بنویسید که نام و نام خانوادگی و سن کاربر را از ورودی دریافت نماید
+# و سپس پیغامی مشابه زیر نمایش دهد
+# 
+# Hello Mary, your family is Rezaei and your age is 15.
