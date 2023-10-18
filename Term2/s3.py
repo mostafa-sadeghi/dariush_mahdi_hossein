ca65bc369952db803d3aff6de0c159fdf9f71a3f
msa <msai.sadeghi@gmail.com>
2023-10-18 21:58:23 +0330

    s

---
+++ Term2/s3.py
@@ -0,0 +1,49 @@
+# TODO
+"""
+تمرین اول
+
+تعداد کاراکترهای متن بالارا نمایش دهید
+تعداد کاراکترهای غیر از فاصله
+را نمایش دهید
+
+تعداد کلمه 
+fox
+در متن بالا را محاسبه و نمایش دهید
+
+"""
+# message1 = "the fox is looking for another fox to be friends with that fox"
+# print(len(message1))
+
+# print(message1.count("fox"))
+# def my_count(mess, word):
+#     c = 0
+#     i = 0
+#     while i < len(mess):
+#         if mess[i:i+len(word)] == word:
+#             c += 1
+#         i += 1
+#     return c
+# print(my_count(message1, "fox"))
+
+
+# def my_function(message):
+#     number_of_chars_except_space = 0
+#     for x in message:
+#         if x != " ":
+#             number_of_chars_except_space += 1
+#     return number_of_chars_except_space
+
+# print(my_function(message1))
+
+# message2 = "here is python class."
+# print(my_function(message2))
+
+# TODO
+"""
+numbers = [28,25,42,2,28]
+می خواهیم کوچکترین و بزرگترین عدد لیست بالا را پیدا کنیم
+برنامه را به صورت تابع بنویسید
+
+"""
+def find_min_max(n):
+    pass
